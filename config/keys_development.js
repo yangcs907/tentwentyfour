945af3510e00f5665f5a516da43aefa1abb327fa
yangcs907 <>
2019-05-27 15:51:52 -0400

    updates

--- config/keys_development.js
+++
@@ -1,5 +0,0 @@
-// keys for development // make sure to include in .gitignore
-module.exports = {
-  mongoURI: 'mongodb://yangcs907:stonecold1@ds221405.mlab.com:21405/tentwentyfour',
-  secretOrKey: 'secret'
-};
